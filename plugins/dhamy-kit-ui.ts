57b5a1882fa675d5620b635c41853377ab6c57b3
Valeria Manriquez <vmanriquez@sourcing.cl>
2025-06-12 14:21:54 -0400

    Instalación de plugin dhamy-kit

--- plugins/dhamy-kit-ui.ts
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from "#app";
-import DhamyKitUi from "dhamy-kit-ui";
-import "dhamy-kit-ui/dhamy-kit-ui.css";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(DhamyKitUi);
-});
